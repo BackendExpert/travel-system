6eb67ba1f48a6201958e0cb67c745c3d4de3916d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-12-15 21:31:52 +0530

    Update

--- client/src/component/Error/DefultError.jsx
+++ client/src/component/Error/DefultError.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { FaExclamationTriangle } from 'react-icons/fa';
+import { Link } from 'react-router-dom'; 
 
 const DefultError = () => {
     return (
-        <div>DefultError</div>
-    )
-}
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 px-4">
 
-export default DefultError
+            <FaExclamationTriangle className="text-red-500 text-8xl mb-6 animate-bounce" />
+
+
+            <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
+
+
+            <p className="text-xl text-gray-600 mb-6 text-center">
+                Oops! The page you are looking for does not exist.
+            </p>
+
+
+            <Link
+                to="/"
+                className="inline-block bg-red-500 text-white px-6 py-3 rounded-lg font-semibold hover:bg-red-600 transition"
+            >
+                Go Back Home
+            </Link>
+        </div>
+    );
+};
+
+export default DefultError;
--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -3,13 +3,15 @@ import WebSite from '../layouts/WebSite'
 import Popups from '../pages/Tests/Popups'
 import ModelPopup from '../pages/Tests/ModelPopup'
 import FormTest from '../pages/Tests/FormTest'
+import DefultError from '../component/Error/DefultError'
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<WebSite />} >
-                    <Route index element={<FormTest /> } />
+                    <Route path='*' element={<DefultError /> } />
+                    <Route index element={<FormTest /> } /> 
                     <Route path='/model' element={<ModelPopup /> } />
                     <Route path='notifications' element={<Popups /> } />
                 </Route>
